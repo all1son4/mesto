ae1c3221c2aa3d9ff99fb3a5a4d1491aa6105539
Dmitry Strok <dm.strok98@gmail.com>
2021-08-17 18:16:25 +0300

    feat: start develop JS enteractive

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+let openPopupButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close-button');
