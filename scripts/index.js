10c44083f3c6e50c893b3518953851d9faa29cca
Dmitry Strok <dm.strok98@gmail.com>
2021-08-20 17:07:37 +0300

    feat: finish popup form

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,37 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('#form');
+let nameInput = document.querySelectorAll('.popup__field')[0];
+let descriptionInput = document.querySelectorAll('.popup__field')[1];
+let popupSubmitButton = document.querySelector('.popup__submit-button');
+
+function switchPopup() {
+  if (!popup.classList.contains('popup_opened')) {
+    nameInput.value = document.querySelector('.profile__name').textContent;
+    descriptionInput.value = document.querySelector('.profile__description').textContent;
+  }
+
+  popup.classList.toggle('popup_opened');
+}
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+
+  let name = document.querySelector('.profile__name');
+  let description = document.querySelector('.profile__description')
+
+  name.textContent = nameInput.value;
+  description.textContent = descriptionInput.value;
+
+  popup.classList.toggle('popup_opened');
+}
+
+openPopupButton.addEventListener('click', switchPopup);
+closePopupButton.addEventListener('click', switchPopup);
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+$(window).load(function() {
+  $("body").removeClass("page__load");
+});
