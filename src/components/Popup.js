12478d07739d623978e27bc13373f564458d17e7
all1son4 <dm.strok98@gmail.com>
2021-10-19 20:09:18 +0300

    ref: end OOP refactoring and Webpack tool

---
+++ src/components/Popup.js
@@ -0,0 +1,30 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popupElement = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._popupElement.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose)
+  }
+
+  close() {
+    this._popupElement.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose)
+  }
+
+  _handleEscClose(event) {
+    if (event.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popupElement.addEventListener('click', (event) => {
+      if (event.target.classList.contains('popup') || event.target.classList.contains('popup__close-button')) {
+        this.close()
+      };
+    })
+  }
+}
