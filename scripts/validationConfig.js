cdbf091c578f2e14c5ba3d50929a8672353f97ab
all1son4 <78477841+all1son4@users.noreply.github.com>
2021-10-19 23:33:28 +0300

    Delete scripts directory

--- scripts/validationConfig.js
+++
@@ -1,8 +0,0 @@
-export const validationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-button',
-  inactiveButtonClass: 'popup__button_invalid',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'error_visible'
-};
